bd5093e4f60d5b08a9d816e6ccec0b613edd00fc
Munshi91 <mimunshi91@gmail.com>
2024-05-28 12:51:51 +0600

    Hero Section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,3 +1,7 @@
+import Swiper from 'swiper';
+import { Authors, Books } from '../data.js';
+import { SwiperSlide } from 'swiper/react';
+
 export const Hero = () => {
   return (
     <>
@@ -12,8 +16,23 @@ export const Hero = () => {
             within shape your world.
           </p>
         </div>
-        <img src="" alt="" />
+        <div className="w-72 ">
+          <swiper-container effect="cards" autoplay="true">
+            {Books.map((book, i) => (
+              <swiper-slide>
+                <img src={book.book_cover} alt={book.title} width="200px" />
+              </swiper-slide>
+            ))}
+          </swiper-container>
+        </div>
       </div>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path
+          fill="#D3D5D1"
+          fillOpacity="1"
+          d="M0,192L26.7,197.3C53.3,203,107,213,160,218.7C213.3,224,267,224,320,197.3C373.3,171,427,117,480,96C533.3,75,587,85,640,122.7C693.3,160,747,224,800,213.3C853.3,203,907,117,960,80C1013.3,43,1067,53,1120,80C1173.3,107,1227,149,1280,154.7C1333.3,160,1387,128,1413,112L1440,96L1440,320L1413.3,320C1386.7,320,1333,320,1280,320C1226.7,320,1173,320,1120,320C1066.7,320,1013,320,960,320C906.7,320,853,320,800,320C746.7,320,693,320,640,320C586.7,320,533,320,480,320C426.7,320,373,320,320,320C266.7,320,213,320,160,320C106.7,320,53,320,27,320L0,320Z"
+        ></path>
+      </svg>
     </>
   );
 };
